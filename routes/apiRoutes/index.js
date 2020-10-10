d3d0c4efa9f14f32435d55c7cba03ba35a2760ff
Seth Uschuk <suschuk24@gmail.com>
2020-10-10 11:34:24 -0500

    added basic GET and POST commands, added functions in lib/notes.js

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,24 +1,27 @@
 const router = require('express').Router();
+const { createNewNote, validateNote } = require('../../lib/notes')
 const { notes } = require('../../db/db.json');
 
+
 router.get('/api/notes', (req, res) => {
-    let results = notes;
-    if(req.query) {
-        results = filterByQuery(req.query, results);
+    const result = createNewNote(req.body, notes);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
     }
-    res.json(results);
-});
+  });
 
 router.post('/api/notes', (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = notes.length.toString();
   
     // if any data in req.body is incorrect, send 400 error back
-    if (!validateAnimal(req.body)) {
-      res.status(400).send('The animal is not properly formatted.');
+    if (!validateNote(req.body)) {
+      res.status(400).send('This note is not properly formatted.');
     } else {
-      const animal = createNewAnimal(req.body, animals);
-      res.json(animal);
+      const note = createNewNote(req.body, notes);
+      res.json(note);
     }
   });
 
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -3,11 +3,11 @@ const router = require('express').Router();
 
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
   });
 
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+router.get('/notes.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
   });
 
 module.exports = router;
