7f693406261db9da7f0d9aae0a169ebba8133032
Seth Uschuk <suschuk24@gmail.com>
2020-10-11 12:13:31 -0500

    added delte function

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -4,6 +4,7 @@ const { createNewNote, validateNote, findById } = require('../../lib/notes')
 const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 
+// retrieve note info from db.json
 router.get('/notes', (req, res) => {
     const result = notes;
     
@@ -13,7 +14,7 @@ router.get('/notes', (req, res) => {
       res.sendStatus(404);
     }
   });
-  
+// ability to find notes by ID for delete function.   
 router.get( '/notes/:id', (req, res) => {
     const result = findById(req.params.id, notes);
 
@@ -24,6 +25,7 @@ router.get( '/notes/:id', (req, res) => {
     };
 });
 
+// collect user input data, store and write to page and db.json
 router.post('/notes', (req, res) => {
     let noteId = uuidv4()
 
@@ -32,15 +34,26 @@ router.post('/notes', (req, res) => {
         title: req.body.title,
         text: req.body.text
     }
-
+    // update sent to array
     notes.push(newNote)
-
+    // update sent to db.json
     res.json(newNote)
-    fs.writeFileSync("./db/db.json", JSON.stringify(notes, null, 2), function (
-        err
-      ) {
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes, null, 2), (err) => {
         if (err) throw err;
       });
+    });
+
+router.delete("/notes/:id", (req, res) => {
+    //find the id of the note that is going to be deleted
+    let deleteNote = notes.findIndex((item) => item.id === req.params.id);
+    //remove the note from the notes array
+    notes.splice(deleteNote, 1);
+    
+    //write the updated array to db.json
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes, null, 2), (err) => {
+        if (err) throw err;
+    });
+    res.json({ deletion: "Note Deleted!" });
     });
 
 module.exports = router;
